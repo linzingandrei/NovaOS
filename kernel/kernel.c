9931456237f24df676b2894e28425de3821dca85
linzingandrei <andrei.linzing@gmail.com>
2025-05-01 14:27:53 +0300

    changes

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,9 +4,9 @@
 void main() {
     clear_screen();
 
-    // print_at("X", 6, 1);
-    print_at("This text spans multiple lines", 75, 10);
-    // print_at("There is a line\nbreak", 20, 0);
-    // print("There is a line\nbreak");
-    // print_at("What happens when we run out of space?", 24, 45);
+    print_at("X", 6, 1);
+    print_at("This text spans multiple lines", 10, 75);
+    print_at("There is a line\nbreak", 20, 0);
+    print("There is a line\nbreak");
+    print_at("What happens when we run out of space?", 24, 45);
 }
